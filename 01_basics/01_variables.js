a8e9334bc63cd3a776b2f7865b541b1ba14ccf22
mohit-bharti <begooddogood719@gmail.com>
2024-08-04 08:29:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const firstName = 'Mohit'
+const lastName = 'Bharti'
+let age = 27
+let gender = "male" 
+const score = 400
+let coder = false
+
+//number 
+//bigInt
+//string
+//boolean
+//null
+//undefiend
+//symbol
+
+//console.log(typeof firstName);
+
