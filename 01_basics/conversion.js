b847747cebd1ab711ad107df078e8fa313ad8609
mohit-bharti <begooddogood719@gmail.com>
2024-08-04 08:55:59 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let likes = 20
+let subscribers = 1000
+let views = "100k"
+let isFollowed = "1"
+let comments = null
+let share = undefined
+
+//let newlikes = String(likes)
+let isFollow =Boolean(isFollowed)
+
+console.log(typeof likes);
+console.log(typeof isFollow)
+
